0feec296f48466ee235787eb39223138db253daf
wilburforce83 <wilburforce83@gmail.com>
2023-09-27 19:39:56 +0100

    refactoring for better future proofing

--- engine/translateEngine.js
+++ engine/translateEngine.js
@@ -5,37 +5,35 @@ const path = require('path');
 
 // translation engine
 module.exports = {
-  
-Engine: function(string, droid, mood) {
-    var wavArray = [];
-    var whichDroid = "";
-    var format = ".wav";
 
-    if (droid == "astro") {
-        whichDroid = '../sounds/astro/';
-    };
-    if (droid == "bd1" && mood == "happy") {
-        whichDroid = '../sounds/bd1/Happy/';
-    };
-    if (droid == "bd1" && mood == "sad") {
-        whichDroid = '../sounds/bd1/Sad/';
-    };
-    if (droid == "bd1" && mood == "angry") {
-        whichDroid = '../sounds/bd1/Angry/';
-    };
-    for (let i = 0; i < string.length; i++) {
-        let character = string[i].toLowerCase();
-        let element = whichDroid + character + format;
-        console.log(whichDroid)
-        wavArray.push(element);
+    Engine: function (string, droid, mood) {
+        var wavArray = [];
+        var whichDroid = "";
+        var format = ".wav";
+        var subDir = mood;
 
-        // Match character to file name somehow
-        // Then append wavArray with the matching wav file.
-    }
-    console.log(wavArray);
-    playWav(wavArray);
-},
-
+        // check if a mood has been selected
+        if (subDir == undefined) {
+            subDir = "";
+        } else {
+            subDir = `${subDir}/`;
+        };
+        //generate folder location
+        if (droid) {
+            whichDroid = `../sounds/${droid}/${subDir}`;
+        } else {
+            console.log('No droid chosen.');
+        };
+        // for each character in the string, generate file location and push to array
+        for (let i = 0; i < string.length; i++) {
+            let character = string[i].toLowerCase();
+            let element = whichDroid + character + format;
+            console.log(whichDroid)
+            wavArray.push(element);
+        };
+        // console.log(wavArray);
+        playWav(wavArray); // sned array to speaker.
+    },
 };
 
 
